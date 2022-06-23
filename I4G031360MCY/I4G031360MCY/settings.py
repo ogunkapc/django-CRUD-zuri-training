7cdbc6dac8f3dec8ba8c60dd04eb412c7e010d8a
Ogunka Promise <ogunkapc@gmail.com>
2022-06-23 17:56:29 +0100

    remove secrt key

--- I4G031360MCY/I4G031360MCY/settings.py
+++ I4G031360MCY/I4G031360MCY/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-icqw(injj)7-!*&6xo%^7s=0e)c)h1m3f(u3zit-qs8eqw24gq'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
